78c8bf5bf5cfc23180f6715e9e6e35b6a306a844
Addrick <asantos@wpi.edu>
2020-07-15 22:50:39 -0700

    added skeleton n stuff

--- robot_with_vision/nodes/PandaController.py
+++ robot_with_vision/nodes/PandaController.py
@@ -17,17 +17,34 @@ from nav_msgs.srv import GetPlan
 class PandaController:
 
     def __init__(self):
+        self.joint_update_delay = 10  # sleep time in ms between sending new joint position
 
-        current_joints = [0, 0, 0, 0, 0, 0, 0]
+        self.current_joints = [0, 0, 0, 0, 0, 0, 0]
 
-        rospy.init_node("panda_control", anonymous=True)
-        rospy.Publisher("/joint_states", JointState, self.joints_update)
+        rospy.init_node("panda_control")
+        rospy.Publisher("/joint_states", JointState)
         rospy.Subscriber('/panda_waypoints', GetPlan, self.request_trajectory)
 
-        self.ros_rate = rospy.Rate(self.rate)
         rospy.loginfo("PandaController server node ready.")
 
     def joints_update(self, joints_msg):
+        # compose joint_state message for each joint and publish them
         return
 
+    def go_to(self, trajectory):
+        # iterate through each trajectory and set each series joint positions
+        for i in range(len(trajectory)):
+            self.joints_update(trajectory[i])
+            rospy.sleep(self.joint_update_delay)
+
+    def request_trajectory(self, midpoint, goal):  # probably will be easier to use some ros data type
+        start = self.current_joints
+        # send message to trajectory node
+        # wait for response
+        # start go_to function
+        return
+
+
+if __name__ == '__main__':
+    panda = PandaController()
 
